7fc25e9bce164b0f1dedffd20f9d19d53a705ec6
JoseJRVazquez <kwikteck@gmail.com>
2014-04-06 20:33:15 -0500

    initial add

---
+++ playtoy.rb
@@ -0,0 +1,41 @@
+#The following is a test file where I am putting methods I have been trying
+#and will likely be useful in developing the website MVP I am working on
+
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+#The follow is just my Hello World Method to say hello to the user
+def hello (first, last)
+  "So your name is #{first_name}, #{last_name}"
+end
+
+p hello ("Jose", "Vazquez")
+
+#This is a link method
+
+def link_to (link, site)  #This sets up the dual arguments of the site and the link
+  "Assignment 1 = <a href='#{site}'>#{link}</a>" #this forumla set up how it should output the arguments
+end #this closes the method
+
+#these are the variables defined for each one
+p link_to("Google", "http://www.google.com") 
+p link_to("Bloc", "http://www.bloc.io")
+
+#Im not sure how I will use this, but the structure is similar to what I will need to 
+#get the due date displayed on the item
+require 'date'
+
+def next_birthday(birthday)
+  next_year = birthday.next_year
+  next_year.strftime("%D")
+end
+
+next_birthday(Date.new(2013,6,1))
+
+
+
