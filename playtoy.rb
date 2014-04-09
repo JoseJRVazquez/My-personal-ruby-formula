91bc73fdcf4e4d6f1cb2584dcfea1c40a37bb3ad
JoseJRVazquez <kwikteck@gmail.com>
2014-04-09 10:52:25 -0500

    added formula for later

--- playtoy.rb
+++ playtoy.rb
@@ -38,4 +38,28 @@ end
 next_birthday(Date.new(2013,6,1))
 
 
+#lock/unlock script
+
+def lock(a,b,c,d)
+  if (a == 3 || a == 5 || a ==7) && (b == 2) && (c == 5 || c == 6) && (d == 8 || d == 9 || d == 0)
+  "unlocked"
+  else
+  "locked"
+end
+end
+
+
+
+#treu false script
+
+def can_i_get?(item, money)
+  if (item == "computer" && money >= 1000)
+  true
+  elsif (item == "ipad" && money >= 500)
+  true
+  else
+  false
+end
+end
+
 
